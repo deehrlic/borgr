6137c9f2743cfb636301cb718a6e7fe2d56f4366
deehrlic <deehrlic@ucsc.edu>
2020-05-02 12:09:02 -0700

    basic flask

--- borgr.py
+++ borgr.py
@@ -2,33 +2,48 @@ import socket, requests
 import os
 from radar import RadarClient
 import re
+from flask import Flask
+import webbrowser
 
-host = socket.gethostname()
-ipU = socket.gethostbyname(host)
+app = Flask(__name__)
 
-print(ipU)
+@app.route('/')
+def home():
 
+    host = socket.gethostname()
+    ipU = socket.gethostbyname(host)
 
+    print(ipU)
 
-# initialize client
-radar = RadarClient('prj_test_pk_d75f2dd9a1887939b58e7b9dcbf4c3c81e0f47d2')
+    # initialize client
+    radar = RadarClient('prj_test_pk_d75f2dd9a1887939b58e7b9dcbf4c3c81e0f47d2')
 
-#geocode, ip = ipU
-cod = radar.geocode.ip(ip='107.77.199.117')
-print(cod.latitude)
-print(cod.longitude)
-user_location=(cod.latitude,cod.longitude)
-search = radar.search.places(near=user_location, categories="burger-restaurant")
-print(search[0].name)
-name = search[0].name
-addr = radar.search.autocomplete(name, near=user_location)
-print(addr[0].formattedAddress)
+    #geocode, ip = ipU
+    cod = radar.geocode.ip(ip='107.77.199.117')
+    print(cod.latitude)
+    print(cod.longitude)
+    user_location=(cod.latitude,cod.longitude)
+    search = radar.search.places(near=user_location, categories="burger-restaurant")
+    print(search[0].name)
+    name = search[0].name
+    addr = radar.search.autocomplete(name, near=user_location)
+    print(addr[0].formattedAddress)
 
-toFormat = str(name)
-toFormat = toFormat.lower()
-toFormat = toFormat.replace(" ","+")
-print(toFormat)
+    toFormat = str(name)
+    toFormat = toFormat.lower()
+    toFormat = toFormat.replace(" ","+")
+    print(toFormat)
 
-addr = "https://www.google.com/maps/search/"+toFormat+"/@"+str(cod.latitude)+","+str(cod.longitude)
-print(addr)
+    addr = "https://www.google.com/maps/search/"+toFormat+"/@"+str(cod.latitude)+","+str(cod.longitude)
+    print(addr)
+
+    #webbrowser.open_new_tab(addr)
+
+    return addr
+
 #https://www.google.com/maps/search/branded+burger+co/@32.5131302,-96.9722533
+
+#67.180.133.255
+
+if __name__ == "__main__":
+    app.run(debug=True)
